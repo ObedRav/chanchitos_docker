3ce39b31ddf66e7ed1588764b5b4d823acc49e06
Obed Rayo <obedrayo2356@gmail.com>
2023-05-16 20:32:35 -0500

    Adding Files

---
+++ index.js
@@ -0,0 +1,31 @@
+import express from 'express'
+import mongoose from 'mongoose'
+
+const Animal = mongoose.model('Animal', new mongoose.Schema({
+  tipo: String,
+  estado: String,
+}))
+
+const app = express()
+
+try {
+  mongoose.connect('mongodb://nico:password@monguito:27017/miapp?authSource=admin')
+  console.log("Database connection established")
+} catch {
+  console.log("Conexion error")
+}
+
+
+app.get('/', async (_req, res) => {
+  console.log('listando... chanchitos...')
+  const animales = await Animal.find();
+  return res.send(animales)
+})
+
+app.get('/crear', async (_req, res) => {
+  console.log('creando...')
+  await Animal.create({ tipo: 'Chanchito', estado: 'Feliz' })
+  return res.send('ok')
+})
+
+app.listen(3000, () => console.log('listening...'))
